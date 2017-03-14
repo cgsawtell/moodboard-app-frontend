439b4c870c799671f5d303ba99dc370c0f08ee04
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-03-14 17:33:27 +1300

    removed logging

--- src/utils/pinky.js
+++ src/utils/pinky.js
@@ -1,11 +1,9 @@
 import {forEach} from 'lodash'
 
 const pinky = (func, context) => {
-  console.log('pinky!',func);
     const handlePromise = (resolve, reject) => {
       func.call(context,
         (...callbackArgs) => {
-          forEach(callbackArgs, arg => console.log(arg))
           resolve(callbackArgs)
         }
       )
