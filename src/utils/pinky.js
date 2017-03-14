20213a57f50136a9be6cb6e1a53922e437444aa5
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-03-14 22:34:11 +1300

    basic js doc

--- src/utils/pinky.js
+++ src/utils/pinky.js
@@ -1,5 +1,9 @@
-import {forEach} from 'lodash'
-
+/**
+ * A utility function that wraps a callback style function in a promise.
+ * @param {*callback function that need to be wrapped in promise} func 
+ * @param {*context that the function is run in} context 
+ * @returns promise
+ */
 const pinky = (func, context) => {
     const handlePromise = (resolve, reject) => {
       func.call(context,
