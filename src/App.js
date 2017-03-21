d33de27cd0fb1d02cb73b9e8191543eedd4010c3
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-03-21 14:15:17 +1300

    added links for login and create accound on default page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Link} from 'react-router'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Link to="/login">Login</Link><br/>
+        <Link to="/create-account">Create Account</Link>
       </div>
     );
   }
